bf9110021688fcea869b0c1cf548560a829c8a3a
Igor Malyarov <igor1309@gmail.com>
2020-07-14 13:27:47 +0300

    Initial Commit

---
+++ MarginApp/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  MarginApp
+//
+//  Created by Igor Malyarov on 14.07.2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MarginApp/MarginAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarginAppApp.swift
+//  MarginApp
+//
+//  Created by Igor Malyarov on 14.07.2020.
+//
+
+import SwiftUI
+
+@main
+struct MarginAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
